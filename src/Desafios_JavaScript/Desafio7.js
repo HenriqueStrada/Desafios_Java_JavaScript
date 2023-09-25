f47128f6cd977285178657a5bca57908d08c8830
Henrique Strada <henriquestradax@gmail.com>
2023-09-25 20:21:40 -0300

    first commit

---
+++ src/Desafios_JavaScript/Desafio7.js
@@ -0,0 +1,33 @@
+desafio7 = ()=> {
+
+    var valor = []
+    for (let i = 1; i <= 5; i++) {
+        var num = prompt(`Digite o seu ${i} valor (apenas numeros inteiros + ou -`);
+        valor.push(num)
+    }
+    console.log(valor)
+    var positivo = 0;
+    var negativo = 0;
+    var par = 0;
+    var impar = 0;
+
+    for (var num of valor) {
+        console.log(num);
+        if (num % 2 == 0){
+            par++;
+        } else {
+            impar++
+        }
+        if (num > 0) {
+            positivo++;
+        } else if (num < 0) {
+            negativo++;
+        }
+    }
+
+    alert(`Em seus numeros contem: ${par} numeros pares, ${impar} impares, ${positivo} positivos, ${negativo} negativos.`)
+    console.log("Par: " + par);
+    console.log("Impar: " + impar);
+    console.log("Positivo: " + positivo);
+    console.log("Negativo: " + negativo);
+}
