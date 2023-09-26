c55cbbb84a36b80604f84636de35d88779e6c8eb
Henrique Strada <henriquestradax@gmail.com>
2023-09-26 19:16:05 -0300

    commit

--- src/Desafios_JavaScript/Desafio8.js
+++ src/Desafios_JavaScript/Desafio8.js
@@ -1,6 +1,5 @@
-desafio8 = ()=>{
-
-    alert("Reserva de ingressos - Clube JavaScript Night")
+desafio8 = () => {
+    alert("Reserva de ingressos - Clube JavaScript Night");
     var nome = prompt("Digite o seu nome: ");
     var idade = prompt("Digite a sua idade: ");
     var tipoEstudante = prompt("Você é estudante de JavaScript? s (de sim); n (de não)");
@@ -9,11 +8,20 @@ desafio8 = ()=>{
     2.VIP R$ 50,00
     Promoção: Ser estudande de JavaScript recebe 50% de desconto!
     `);
-    if(tipoIngresso){
-        if(idade >= 18){
-            if(tipoIngresso == 1){
 
-            }
-        }
+    if (idade < 18) {
+        alert("Não é permitido a entrada de menores de idade.");
+        var idadeNova = 18 - idade;
+        alert(`Ainda faltam ${idadeNova} anos para participar das festas.`)
+        return;
     }
+
+    var valorIngresso = tipoIngresso == 1 ? 20 : 50;
+    var valorDescontado = valorIngresso;
+
+    if (tipoEstudante.toLowerCase() == 's') {
+        valorDescontado *= 0.5;
+    }
+
+    alert(`O valor original do ingresso é R$ ${valorIngresso.toFixed(2)}, e o valor com desconto é R$ ${valorDescontado.toFixed(2)}.`);
 }
